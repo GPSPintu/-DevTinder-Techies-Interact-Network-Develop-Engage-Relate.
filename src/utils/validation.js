f47719753a0da2a7e922d7ed3e0f53b25e2c55dd
GPSPintu <pintugps.in@gmail.com>
2025-11-14 05:42:58 +0530

    utils: validation of users

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,6 +1,7 @@
 // Import the 'validator' library for validating strings like emails and passwords
 const validator = require("validator");
 
+
 // Function to validate user data during sign-up
 const validateSignUpData = (req) => {
   // Destructure the required fields from the request body
@@ -8,17 +9,18 @@ const validateSignUpData = (req) => {
 
   // Check if the user's first and last name are provided
   if (!firstName || !lastName) {
-    throw new Error("Name is not valid!"); // Throw an error if name fields are missing
+    throw new Error("Name is not valid!");             // Throw error if name fields are missing
   } 
   // Check if the provided email is valid
   else if (!validator.isEmail(emailId)) {
-    throw new Error("Email is not valid!"); // Throw an error if email is invalid
+    throw new Error("Email is not valid!");            // Throw error if email is invalid
   } 
-  // Check if the password is strong enough (contains uppercase, lowercase, number, symbol)
+  // Check if the password is strong enough (uppercase, lowercase, number, symbol)
   else if (!validator.isStrongPassword(password)) {
-    throw new Error("Please enter a strong Password!"); // Throw an error if password is weak
+    throw new Error("Please enter a strong Password!"); // Throw error if password is weak
   }
 };
+
 
 // Function to validate which fields can be edited in a user profile
 const validateEditProfileData = (req) => {
@@ -43,9 +45,9 @@ const validateEditProfileData = (req) => {
   return isEditAllowed;
 };
 
+
 // Export both validation functions for use in other files
 module.exports = {
   validateSignUpData,
   validateEditProfileData,
 };
-
